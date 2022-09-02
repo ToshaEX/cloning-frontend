4773c04137c8b14cc146a4f86a80891290a4beda
ToshaEX <tharinduoshan@gmail.com>
2022-09-02 15:26:48 +0530

    Complete Home Page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,37 +9,39 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div
-      className="home"
-      style={{
-        backgroundImage: `url(${homeMain})`,
-        backgroundSize: "cover",
-      }}
-    >
-      <div className="home-top">
-        <img src={logo} alt="Logo" className="top-logo" />
-        <button type="button" className="btn btn-primary">
-          {" "}
-          Sign Out
-        </button>
-      </div>
-      <div className="home-intro">
-        <p className="intro-para">
-          Welcome back!
-          <br />
-          <span className="para-line-1">
-            Unlimited movies, TV shows, and more.
+    <>
+      <div
+        className="home"
+        style={{
+          backgroundImage: `url(${homeMain}),linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 100%)`,
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="home-top">
+          <img src={logo} alt="Logo" className="top-logo" />
+          <button type="button" className="btn btn-primary">
+            {" "}
+            Sign Out
+          </button>
+        </div>
+        <div className="home-intro">
+          <p className="intro-para">
+            Welcome back!
             <br />
-          </span>
-          <span className="para-line-2">Watch anywhere. Cancel anytime.</span>
-        </p>
-        <button type="button" className="btn btn-primary">
-          {" "}
-          Finish Sign Up
-        </button>
+            <span className="para-line-1">
+              Unlimited movies, TV shows, and more.
+              <br />
+            </span>
+            <span className="para-line-2">Watch anywhere. Cancel anytime.</span>
+          </p>
+          <button type="button" className="btn btn-primary finish">
+            {" "}
+            Finish Sign Up
+          </button>
+        </div>
       </div>
-      <hr className="divider" />
-    </div>
+      <div className="divider" />
+    </>
   );
 };
 
